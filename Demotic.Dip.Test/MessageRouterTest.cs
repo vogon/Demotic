8f9326cd2e4722402b44793bc2bea7b4cff5df92
Colin Bayer <vogon@icculus.org>
2011-12-11 15:24:21 -0800

    probably totally-busted commit so that I can switch over to the working copy on my desktop because I forgot to bring my laptop AC adapter home this weekend #firstworldproblems

---
+++ Demotic.Dip.Test/MessageRouterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Demotic.Core.ObjectSystem;
+
+namespace Demotic.Dip.Test
+{
+    [TestClass]
+    public class MessageRouterTest
+    {
+        [TestMethod]
+        public void TestEmptyRouter()
+        {
+            MessageRouter<int> router = new MessageRouter<int>();
+            DNumber x = 3;
+
+        }
+    }
+}
---
+++ Demotic.Dip/MessageRouter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Demotic.Core.ObjectSystem;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace Demotic.Dip
+{
+    public class MessageRouter<T>
+    {
+        public MessageRouter()
+        {
+            _chains = new List<VerifyChain>();
+        }
+
+        public void AddVerifier(T routingInfo, Func<dynamic, bool> verifier)
+        {
+            VerifyChain chain = _chains.FirstOrDefault(c => c.RoutingInfo.Equals(routingInfo));
+
+            if (chain == null)
+            {
+                chain = new VerifyChain();
+                _chains.Add(chain);
+                chain.RoutingInfo = routingInfo;
+            }
+
+            chain.Verifiers.Add(verifier);
+        }
+
+        public T Lookup(DObject msg)
+        {
+            foreach (VerifyChain chain in _chains)
+            {
+                bool accept = true;
+
+                foreach (var f in chain.Verifiers)
+                {
+                    try
+                    {
+                        if (!f(msg))
+                        {
+                            accept = false;
+                            break;
+                        }
+                    }
+                    catch (RuntimeBinderException)
+                    {
+                        accept = false;
+                        break;
+                    }
+                }
+
+                if (accept)
+                {
+                    return chain.RoutingInfo;
+                }
+            }
+
+            return default(T);
+        }
+
+        private class VerifyChain
+        {
+            public VerifyChain()
+            {
+                RoutingInfo = default(T);
+                Verifiers = new List<Func<dynamic, bool>>();
+            }
+
+            public T RoutingInfo { get; set; }
+            public List<Func<dynamic, bool>> Verifiers { get; set; }
+        }
+
+        private List<VerifyChain> _chains;
+    }
+}
